d2b634218e4c9f4485153305b21158b7f3b32e58
sasijagtap <157822731+sasijagtap@users.noreply.github.com>
2024-01-27 08:18:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello, World!";
     }
 }
